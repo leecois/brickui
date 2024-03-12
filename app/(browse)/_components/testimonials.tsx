be6b3e31884a413949042b4b96bb972bbfd5a723
HCM24_CPL_REACT_02_LeQuocKhanh <khanhlqse170317@fpt.edu.vn>
2024-03-12 21:21:37 +0700

    fix bug

--- app/(browse)/_components/testimonials.tsx
+++ app/(browse)/_components/testimonials.tsx
@@ -9,7 +9,8 @@ export function Testimonials() {
   return (
     <Section crosses className='!px-10 !py-0'>
       <div className='relative flex h-screen flex-col items-center justify-center overflow-hidden rounded-md bg-black antialiased dark:bg-black'>
-        <Meteors number={20} />
+        <Meteors number={50} />
+        <InfiniteMovingCards items={testimonials} direction='right' speed='normal' />
       </div>
     </Section>
   )
