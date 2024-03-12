70c7a3672977312b3b3cbc1a086ddbe93e6fdb74
HCM24_CPL_REACT_02_LeQuocKhanh <khanhlqse170317@fpt.edu.vn>
2024-03-12 20:02:37 +0700

    Update number of meteors in testimonials component

--- app/(browse)/_components/testimonials.tsx
+++ app/(browse)/_components/testimonials.tsx
@@ -9,7 +9,7 @@ export function Testimonials() {
   return (
     <Section crosses className='!px-10 !py-0'>
       <div className='relative flex h-screen flex-col items-center justify-center overflow-hidden rounded-md bg-black antialiased dark:bg-black'>
-        <Meteors number={50} />
+        <Meteors number={15} />
         <InfiniteMovingCards items={testimonials} direction='right' speed='normal' />
       </div>
     </Section>
