22d75ff5bc004a10543b39e8e04b83b4d40bc76f
HCM24_CPL_REACT_02_LeQuocKhanh <khanhlqse170317@fpt.edu.vn>
2024-03-13 14:17:48 +0700

    test

--- app/(browse)/_components/design/moving-cards.tsx
+++ app/(browse)/_components/design/moving-cards.tsx
@@ -1,83 +1,96 @@
-import React, { useEffect, useLayoutEffect, useRef } from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 
-type Item = {
-  quote: string
-  name: string
-  title: string
-  image: string
-}
-
-interface InfiniteMovingCardsProps {
-  items: Item[]
-  direction?: 'left' | 'right'
-  speed?: 'fast' | 'normal' | 'slow'
-  pauseOnHover?: boolean
-  className?: string
-}
-
-export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
+import { cn } from '@/utils'
+export const InfiniteMovingCards = ({
   items,
   direction = 'left',
   speed = 'fast',
   pauseOnHover = true,
-  className = ''
+  className
+}: {
+  items: {
+    quote: string
+    name: string
+    title: string
+    image: string
+  }[]
+  direction?: 'left' | 'right'
+  speed?: 'fast' | 'normal' | 'slow'
+  pauseOnHover?: boolean
+  className?: string
 }) => {
-  const containerRef = useRef<HTMLDivElement>(null)
-  const scrollerRef = useRef<HTMLUListElement>(null)
+  const containerRef = React.useRef<HTMLDivElement>(null)
+  const scrollerRef = React.useRef<HTMLUListElement>(null)
 
-  useLayoutEffect(() => {
-    if (containerRef.current && scrollerRef.current) {
-      duplicateItems()
-      applyDirection()
-      applySpeed()
-    }
+  useEffect(() => {
+    addAnimation()
   }, [])
+  const [start, setStart] = useState(false)
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children)
 
-  const duplicateItems = () => {
-    const scroller = scrollerRef.current
-    if (!scroller) return
+      scrollerContent.forEach(item => {
+        const duplicatedItem = item.cloneNode(true)
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem)
+        }
+      })
 
-    const children = Array.from(scroller.children)
-    children.forEach(child => {
-      const clone = child.cloneNode(true)
-      scroller.appendChild(clone)
-    })
+      getDirection()
+      getSpeed()
+      setStart(true)
+    }
   }
-
-  const applyDirection = () => {
-    const propertyValue = direction === 'left' ? 'forwards' : 'reverse'
-    containerRef.current?.style.setProperty('--animation-direction', propertyValue)
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === 'left') {
+        containerRef.current.style.setProperty('--animation-direction', 'forwards')
+      } else {
+        containerRef.current.style.setProperty('--animation-direction', 'reverse')
+      }
+    }
   }
-
-  const applySpeed = () => {
-    const duration = speed === 'fast' ? '20s' : speed === 'normal' ? '40s' : '80s'
-    containerRef.current?.style.setProperty('--animation-duration', duration)
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === 'fast') {
+        containerRef.current.style.setProperty('--animation-duration', '20s')
+      } else if (speed === 'normal') {
+        containerRef.current.style.setProperty('--animation-duration', '40s')
+      } else {
+        containerRef.current.style.setProperty('--animation-duration', '80s')
+      }
+    }
   }
-
-  const hoverClass = pauseOnHover ? 'hover:[animation-play-state:paused]' : ''
-  const animationClass = 'animate-scroll'
-
   return (
     <div
       ref={containerRef}
-      className={`relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)] ${className}`}>
+      className={cn(
+        'relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]',
+        className
+      )}>
       <ul
         ref={scrollerRef}
-        className={`flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4 ${animationClass} ${hoverClass}`}>
-        {items.map(item => (
+        className={cn(
+          ' flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4',
+          start && 'animate-scroll ',
+          pauseOnHover && 'hover:[animation-play-state:paused]'
+        )}>
+        {items.map((item, idx) => (
           <li
-            key={item.name}
             className='relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-6 md:w-[450px]'
-            style={{ background: 'linear-gradient(180deg, var(--slate-800), var(--slate-900))' }}>
-            <Image
-              src={item.image}
-              width={450}
-              height={300}
-              className='mb-5 h-60 w-full rounded-xl object-cover group-hover:card-shadow-xl'
-              alt='thumbnail'
-            />
+            style={{
+              background: 'linear-gradient(180deg, var(--slate-800), var(--slate-900)'
+            }}
+            key={item.name}>
+
             <blockquote>
+              <div
+                aria-hidden='true'
+                className='pointer-events-none absolute -left-0.5 -top-0.5 size-[calc(100%_+_4px)]'></div>
               <span className='relative z-20 text-sm font-normal leading-[1.6] text-stone-100'>
                 {item.quote}
               </span>
