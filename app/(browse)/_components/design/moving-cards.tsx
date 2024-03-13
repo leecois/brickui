566d68fc5f8a324449b8307b391c35bdc139b038
HCM24_CPL_REACT_02_LeQuocKhanh <khanhlqse170317@fpt.edu.vn>
2024-03-13 15:50:05 +0700

    test deploy

--- app/(browse)/_components/design/moving-cards.tsx
+++ app/(browse)/_components/design/moving-cards.tsx
@@ -1,12 +1,15 @@
 'use client'
 
+import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
-import React, { useEffect, useRef } from 'react'
 
+// Utils for handling classNames more elegantly
 import { cn } from '@/utils'
-import { Avatar } from '@/components/ui/avatar'
+// UI components from your project
 import { Card, CardContent } from '@/components/ui/card'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
 
+// Type definition for props to enhance readability and maintainability
 interface Item {
   quote: string
   name: string
@@ -35,6 +38,7 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
   useEffect(() => {
     duplicateItems()
     applyAnimationSettings()
+    // Enabling start state not needed if its sole purpose is to trigger animations
   }, [])
 
   const duplicateItems = () => {
@@ -58,6 +62,7 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
     }
   }
 
+  // Generate classNames dynamically using Tailwind and utility functions
   const containerClasses = cn(
     'relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]',
     className
@@ -77,19 +82,17 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
             className='relative w-[350px] h-[450px] max-w-full shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-6 md:w-[450px] bg-gradient-to-b from-slate-800 to-slate-900'>
             <Card className='w-full h-full p-6 shadow-lg duration-300 ease-in-out hover:shadow-2xl'>
               <CardContent>
+                <blockquote className='text-lg font-semibold italic'>{item.quote}</blockquote>
                 <div className='flex items-center mt-6 space-x-3'>
                   <Avatar>
-                    <Image alt={item.name} src={item.image} width='450' height='300' />
+                    <AvatarImage alt={item.name} src={item.image} />
                   </Avatar>
                   <div>
                     <div className='font-medium'>{item.name}</div>
                     <div className='text-sm text-gray-600'>{item.title}</div>
-                    <div className='font-bold text-green-600'>BrickAI</div>
+                    <div className='font-bold text-green-600'>Kinsta</div>
                   </div>
                 </div>
-                <blockquote className='text-lg font-semibold italic leading-loose'>
-                  “{item.quote}”
-                </blockquote>
               </CardContent>
             </Card>
           </li>
