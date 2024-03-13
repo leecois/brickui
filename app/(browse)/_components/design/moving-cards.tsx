57f4f92b66e7e19cad202a48bdcf74f30071df8a
HCM24_CPL_REACT_02_LeQuocKhanh <khanhlqse170317@fpt.edu.vn>
2024-03-13 16:27:51 +0700

    Refactor moving-cards component

--- app/(browse)/_components/design/moving-cards.tsx
+++ app/(browse)/_components/design/moving-cards.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react'
-import Image from 'next/image'
+import React, { useEffect, useRef } from 'react'
 
-// Utils for handling classNames more elegantly
 import { cn } from '@/utils'
-// UI components from your project
-import { Card, CardContent } from '@/components/ui/card'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
-// Type definition for props to enhance readability and maintainability
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Card, CardContent } from '@/components/ui/card'
+
 interface Item {
   quote: string
   name: string
@@ -59,8 +56,6 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
       container.style.setProperty('--animation-duration', durationValue)
     }
   }
-
-  // Generate classNames dynamically using Tailwind and utility functions
   const containerClasses = cn(
     'relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]',
     className
@@ -77,12 +72,12 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
         {items.concat(items).map((item, index) => (
           <li
             key={`${item.name}-${index}`}
-            className='relative w-[350px] h-[450px] max-w-full shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-6 md:w-[450px] bg-gradient-to-b from-slate-800 to-slate-900'>
+            className='relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-6 md:w-[450px] bg-gradient-to-b from-slate-800 to-slate-900'>
             <Card className='w-full h-full p-6 shadow-lg duration-300 ease-in-out hover:shadow-2xl'>
               <CardContent>
                 <div className='flex items-center mt-6 space-x-3'>
                   <Avatar>
-                    <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+                    <AvatarImage src={item.image} alt='@shadcn' />
                     <AvatarFallback>CN</AvatarFallback>
                   </Avatar>
                   <div>
@@ -91,7 +86,9 @@ export const InfiniteMovingCards: React.FC<InfiniteMovingCardsProps> = ({
                     <div className='font-bold text-green-600'>BrickAI</div>
                   </div>
                 </div>
-                <blockquote className='text-lg font-semibold italic leading-loose'>“{item.quote}”</blockquote>
+                <blockquote className='text-lg font-semibold italic leading-loose'>
+                  “{item.quote}”
+                </blockquote>
               </CardContent>
             </Card>
           </li>
