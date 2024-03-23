1ff877eac84838600ecedd666d6641a616b6508e
Huh Khanh <lee.cois.dev@gmail.com>
2024-03-23 18:44:33 +0700

    Update header label in auth component

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -11,11 +11,11 @@ interface HeaderProps {
   label: string
 }
 
-export const Header = ({}: HeaderProps) => {
+export const Header = ({ label }: HeaderProps) => {
   return (
     <div className='flex w-full flex-col items-center justify-center gap-y-4'>
-      <h1 className={cn('text-3xl font-semibold', font.className)}>Auth</h1>
-      <p className='text-sm text-muted-foreground'>Welcome Back</p>
+      <h1 className={cn('text-3xl font-semibold', font.className)}>BrickAI</h1>
+      <p className='text-sm text-muted-foreground'>{label}</p>
     </div>
   )
 }
